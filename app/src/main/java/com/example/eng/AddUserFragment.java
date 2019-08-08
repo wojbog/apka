600ba40fffe93efd54d1e8c107ae3151de38a69c
wojbog <wojtek44wojciech@gmail.com>
2019-08-08 12:04:39 +0200

    test

--- app/src/main/java/com/example/eng/AddUserFragment.java
+++ app/src/main/java/com/example/eng/AddUserFragment.java
@@ -51,6 +51,7 @@ public class AddUserFragment extends Fragment {
 
                Username.setText("");
                Usersurname.setText("");
+               //ale jaja
 
            }
        });
