ef68bd03439f6728c6eda1658cdce521cbd3e6d0
PanTajemnic <idziejczak3@gmail.com>
2019-08-16 11:36:18 +0200

    ulepszenie kartkówki o podpowiedzi na toastach

--- app/src/main/java/com/example/eng/MyDao.java
+++ app/src/main/java/com/example/eng/MyDao.java
@@ -21,6 +21,9 @@ public interface MyDao {
 
     @Query("select * from users where kategoria = :cat")
     public List<User> loadUserByKategoria(String cat);
+//
+//    @Query("select kategoria from users LIMIT 1 ")
+//    public List<User> loadKategorie();
 
     @Delete
   public void deleteUsers (User user);
