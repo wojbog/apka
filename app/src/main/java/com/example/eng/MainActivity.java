f319660621afd15ac0124165f3ddd592797c9a5b
PanTajemnic <idziejczak3@gmail.com>
2019-08-19 14:44:43 +0200

    ekran powitalny aplikacji

--- app/src/main/java/com/example/eng/MainActivity.java
+++ app/src/main/java/com/example/eng/MainActivity.java
@@ -6,6 +6,12 @@ import androidx.room.Room;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
   public static FragmentManager fragmentManager;
@@ -22,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
         baza = Room.databaseBuilder(getApplicationContext(),MyappDatabase.class,"BazaDanych").allowMainThreadQueries().build();
         bazaKategorii = Room.databaseBuilder(getApplicationContext(),MyappDatabase.class,"BazaDanychKategorii").allowMainThreadQueries().build();
         Log.d(TAG, "onCreate: zbudowano bazy");
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast_flscr_kartkowka,
+                (ViewGroup) findViewById(R.id.custom_toast_contain));
+        TextView text = layout.findViewById(R.id.text);
+        text.setText("WITAMY W APLIKACJI");
+        layout.setBackgroundResource(R.color.kartkowkaKoniec);
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
 
         if(findViewById(R.id.stefan)!=null)
         {
