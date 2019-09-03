b20005220cc79944f03eb9ad401de617344954c1
PanTajemnic <idziejczak3@gmail.com>
2019-09-03 07:21:34 +0200

    poprawka z add category

--- app/src/main/java/com/example/eng/AddCategoryFragment.java
+++ app/src/main/java/com/example/eng/AddCategoryFragment.java
@@ -45,7 +45,6 @@ public class AddCategoryFragment extends Fragment {
 
 
                 if (
-                        kc("/")||
                         kc("|")||
                         kc("\\")||
                         kc("!")||
